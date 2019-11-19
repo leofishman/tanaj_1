92455dc43d332b9739513f2447d70396e1c92d9b
leofishman <leofishman@gmail.com>
2019-11-18 21:28:44 -0300

    first commit

---
+++ 1/comments.js
@@ -0,0 +1,54 @@
+var bereshit_array, bereshit_comments_array;
+$.getJSON("bereshit-sp-comments-json.js", function(bereshit_comments) {
+    bereshit_comments_array = bereshit_comments;
+
+    $.getJSON("bereshit-sp-json.js", function(bereshit) {
+            bereshit_array = bereshit;
+            bereshit_array.chapter.forEach(chapters);
+
+            function chapters(chapter, chapter_number) {
+                capitulo = '<div class="bereshit-chapter"><h3 class="chapter">Capitulo ' + (chapter_number + 1) + '</h3>';
+                versiculos = '';
+                bereshit_array.chapter[chapter_number].forEach(versicles);
+                function versicles(versicle, versicle_number) {
+                    comment = bereshit_comments_array.chapter[0][chapter_number][versicle_number].toString();
+                    prefix = '<div class="bereshit-versicle"><div class="bereshit-' + (chapter_number + 1) + '-versicle-' + (versicle_number + 1) + '">';
+                    suffix = '</div>';
+                   
+                    suffix += '</div>';
+                    versiculos += prefix + (versicle_number + 1) + ":" + versicle + suffix;
+
+                }
+
+                document.getElementById("bereshit").innerHTML += capitulo + versiculos + '</div>';
+
+            }
+
+        });
+
+});
+
+
+
+document.addEventListener('click', function (e) {
+    console.log(e.target.className);
+    versicle = e.target.className.split('-');
+    console.log(versicle[0],versicle[1],versicle[2],versicle[3]);
+    if (versicle[0] == 'bereshit' && versicle[2] == 'versicle') {
+        coordenada = [parseInt(versicle[1])-1,parseInt(versicle[3])-1];
+        console.log(coordenada, bereshit_array.chapter[(versicle[1]-1)][(versicle[3]-1)],bereshit_comments_array.chapter[(versicle[2]-1)]);
+    }
+    if (hasClass(e.target, 'bereshit')) {
+        // .bu clicked
+        // Do your thing
+        console.log('bereshit');
+    } else if (hasClass(e.target, 'bereshit-versicle')) {
+        // .test clicked
+        // Do your other thing
+        console.log('bereshit-versicle');
+    }
+}, false);
+
+function hasClass(elem, className) {
+    return elem.className.split(' ').indexOf(className) > -1;
+}
